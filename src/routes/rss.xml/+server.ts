5732d9356f4d535037ea4ce76253f8addcc1f613
Jimmy McBride <mcbride967@gmail.com>
2023-09-04 22:16:05 -0500

    created escape function for rss feed

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -1,6 +1,15 @@
 import * as config from "$lib/config"
 import type { ServerLoadEvent } from "@sveltejs/kit"
 
+function escapeXml(unsafe: string): string {
+	return unsafe
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&apos;")
+}
+
 export async function GET({ fetch }: ServerLoadEvent) {
 	const response = await fetch("api/posts")
 	const posts: Post[] = await response.json()
@@ -9,26 +18,30 @@ export async function GET({ fetch }: ServerLoadEvent) {
 
 	const xml = `
 		<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
-			<channel>
-				<title>${config.title}</title>
-				<description>${config.description}</description>
-				<link>${config.url}</link>
-				<atom:link href="${config.url}/rss.xml" rel="self" type="application/rss+xml"/>
-				${posts
-					.map(
-						(post) => `
-						<item>
-							<title>${post.title}</title>
-							<description>${post.description}</description>
-							<link>${config.url}/${post.slug}</link>
-							<guid isPermaLink="true">${config.url}/${post.slug}</guid>
-							<pubDate>${new Date(post.date).toUTCString()}</pubDate>
-						</item>
-					`
-					)
-					.join("")}
-			</channel>
-		</rss>
+            <channel>
+                <title>${escapeXml(config.title)}</title>
+                <description>${escapeXml(config.description)}</description>
+                <link>${escapeXml(config.url)}</link>
+                <atom:link href="${escapeXml(
+									config.url
+								)}/rss.xml" rel="self" type="application/rss+xml"/>
+                ${posts
+									.map(
+										(post) => `
+                        <item>
+                            <title>${escapeXml(post.title)}</title>
+                            <description>${escapeXml(post.description)}</description>
+                            <link>${escapeXml(config.url)}/${escapeXml(post.slug)}</link>
+                            <guid isPermaLink="true">${escapeXml(config.url)}/${escapeXml(
+											post.slug
+										)}</guid>
+                            <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+                        </item>
+                    `
+									)
+									.join("")}
+            </channel>
+        </rss>
 	`.trim()
 
 	return new Response(xml, { headers })

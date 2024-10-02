70afdee5ed336b015bc7dc2e922f0cbe74ed3fe7
Jimmy McBride <mcbride967@gmail.com>
2024-10-01 21:12:16 -0500

    more seo updates

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -102,7 +102,7 @@
 	<!-- Title -->
 	<hgroup class="flex flex-col items-end">
 		<h1 class="">{data.meta.title}</h1>
-		<img src={data.meta.image} alt="blog banner" class="rounded-md" width="800px" loading="lazy" />
+		<img src={data.meta.image} alt="blog banner" class="rounded-md" width="800px" />
 		<p class="text-end text-sm">
 			Published at {formatDate(data.meta.date)}
 			<br />

4eae2511f7275200df0a84096a51aeba5f228bbc
Jimmy McBride <mcbride967@gmail.com>
2024-10-06 16:17:45 -0500

    fixed meta image

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -91,7 +91,7 @@
 	<meta name="twitter:title" content={data.meta.title} />
 	<meta name="twitter:description" content={data.meta.description} />
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:image:src" content={data.meta.image} />
+	<meta name="twitter:image" content={data.meta.image} />
 	<meta name="twitter:widgets:new-embed-design" content="on" />
 
 	<meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />

05954937dae7d8e0a303d6e087eab95517b8d8c0
Jimmy McBride <mcbride967@gmail.com>
2024-10-11 12:27:49 -0500

    added docker support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+	plugins: [sveltekit()],
+	define: {
+		"process.env.FIREBASE_PROJECT_ID": JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+		"process.env.FIREBASE_CLIENT_EMAIL": JSON.stringify(process.env.FIREBASE_CLIENT_EMAIL),
+		"process.env.FIREBASE_PRIVATE_KEY": JSON.stringify(process.env.FIREBASE_PRIVATE_KEY),
+		"process.env.FIREBASE_DATABASE_URL": JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+	},
+})
